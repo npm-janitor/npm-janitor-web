d34b8d5fc70f3a19320502068dbdd4ed8a8b9df4
Hemanth.HM <hemanth.hm@gmail.com>
2015-10-05 18:40:57 +0530

    Config changes and typos fixed

--- main.js
+++ main.js
@@ -21,8 +21,8 @@ class App extends React.Component {
         <img src='http://i.imgur.com/zKKUaav.gif'/>
         <div className="info">
             <h1> npm-janitor </h1>
-            <h3> Enter the npm user name in the search box </h3>
-            <p> Red -> Error, Warnings -> Yellow and Green -> Recomendations </p>
+            <h3> Enter the npm user name in the search box. </h3>
+            <p> Red -> Error, Warnings -> Yellow and Green -> Recommendations. </p>
         </div>
         <DebounceInput
                 minLength={2}

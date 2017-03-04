e02e833f940ad51e02667a0f6c20c8a3fbbb6e55
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-03-04 21:37:50 +0530

    Add request layer

---
+++ api/user.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+const get = (username) => {
+  return axios.get(`https://npm-janitor.herokuapp.com/api/${username}`)
+  .then(response => {
+    let modules = response.data
+    modules = modules.map(module => ({
+      name: module.module,
+      errors: module.info.errors,
+      valid: module.info.valid,
+      warnings: module.info.warnings,
+      recommendations: module.info.recommendations
+    }))
+    return {data: modules}
+  })
+  .catch(error => ({error: error.response.data.message}))
+}
+
+export default get

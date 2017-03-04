e5acfd19e7af128e4b0b7979c752fca78271cf3e
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-03-04 21:35:59 +0530

    Add component for showing helper text

---
+++ components/tiny.js
@@ -0,0 +1,10 @@
+const Tiny = styled.span`
+  color: #666;
+  font-style: italic;
+  position: relative;
+  left: -75px;
+  display: inline-block;
+  opacity: ${props => props.show ? 1 : 0}
+`
+
+export default Tiny

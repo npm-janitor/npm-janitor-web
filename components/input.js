9ffebf0ea265174d988517dde8005b8dd905bf74
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-03-04 21:36:37 +0530

    Input component

---
+++ components/input.js
@@ -0,0 +1,16 @@
+const Input = styled.input`
+  border: none;
+  font-size: 18px;
+  padding: 20px;
+  width: 80vw;
+  margin: 10px;
+  margin-top: -3px;
+  outline: none;
+  color: #333;
+  font-family: 'Lato', sans-serif;
+  @media (max-width: 720px) {
+    width: 90vw;
+	}
+`
+
+export default Input

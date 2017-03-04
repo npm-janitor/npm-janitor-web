a826801a9a69e71f9f7746926abb20495a30845f
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-03-04 21:37:20 +0530

    Canonical card component

---
+++ components/card.js
@@ -0,0 +1,10 @@
+const Card = styled.span`
+  background: #FFF;
+  margin: 10px;
+  padding: 20px;
+  display: inline-block;
+  border-radius: 2px;
+  box-shadow: 0px 1px 3px #DDD;
+`
+
+export default Card
